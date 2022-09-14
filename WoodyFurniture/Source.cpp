c8a9ee5a50077ce242c75c79958d43849bc40e7f
wof8317 <wof8317@gmail.com>
2022-09-14 15:13:06 -0500

    Add project files.

---
+++ WoodyFurniture/Source.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+// fixed point notation
+#include <iomanip>
+using namespace std;
+
+int main() {
+	
+	const double COLONIAL = 85;
+	const double MODERN = 57.5;
+	const double CLASSICAL = 127.75;
+
+	int qtColonial;
+	int qtModern;
+	int qtClassical;
+
+	cout << "Please input the number of American Colonial chairs sold: ";
+	cin >> qtColonial;
+
+	cout << "\nPlease input the number of Modern chairs sold: ";
+	cin >> qtModern;
+
+	cout << "\nPlease input the number of French classical chairs sold: ";
+	cin >> qtClassical;
+
+	double totalColonial = qtColonial * COLONIAL;
+	double totalModern = qtModern * MODERN;
+	double totalClassical = qtClassical * CLASSICAL;
+	double total = totalColonial + totalModern + totalClassical;
+
+	cout << setprecision(2) << fixed; // Fixed decimal notation
+	cout << "\n\nThe total sales for American Colonial chairs: " << totalColonial << endl;
+	cout << "The total sales for Modern chairs:	" << totalModern << endl;
+	cout << "The total sales for French Classical chairs: " << totalClassical << endl;
+	cout << "The total sales for all chairs: " << total;
+
+	return 0;
+}
